e8fb9ddc6756f24260fd6f6e062f411824b2c176
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2023-03-27 17:54:29 -0400

    Connected database to API

--- server_golang/go.mod
+++ server_golang/go.mod
@@ -1,3 +1,18 @@
 module server_golang
 
 go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/godror/godror v0.36.0 // indirect
+	github.com/godror/knownpb v0.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 //indirect
+	github.com/mattn/go-oci8 v0.1.1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- server_golang/main.go
+++ server_golang/main.go
@@ -1,7 +1,75 @@
-package main  
+package main
  
-import "fmt" 
+import (
+    "fmt"
+    "database/sql"
+    _ "github.com/godror/godror"
+)
  
-func main() {  
-   fmt.Println("Hello World")  
+func main(){
+    
+    db, err := sql.Open("godror", `user="ch.lin" password="fh5CyWai7Ppx8aIdELGDUr3m" connectString="oracle.cise.ufl.edu:1521/orcl"`)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer db.Close()
+     
+     /* Just to test if the database is working*/
+
+    // rows,err := db.Query("select balance from account")
+    // if err != nil {
+    //     fmt.Println("Error running query")
+    //     fmt.Println(err)
+    //     return
+    // }
+    // defer rows.Close()
+ 
+    // var value string
+    // for rows.Next() {
+ 
+    //     rows.Scan(&value)
+    // }
+    // fmt.Printf("The date is: %s\n", value)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"server_golang/Config"
+
+// 	"github.com/jinzhu/gorm"
+// 	// _ "github.com/go-sql-driver/mysql"
+// 	_ "github.com/godror/godror"
+// 	// _ "github.com/mattn/go-oci8"
+// )
+
+// var err error
+
+// func main() {
+// 	fmt.Println("Hello World")
+// 	Config.DB, err = gorm.Open("godror", Config.DbURL(Config.BuildDBConfig()))
+// 	if err != nil {
+// 		fmt.Println("Status:", err)
+// 	}
+
+// 	defer Config.DB.Close()
+	
+// }
